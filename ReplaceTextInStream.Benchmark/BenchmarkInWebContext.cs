b807e110bcb0612c4bf08f226b5361f06f7f9aff
Lodewijk Sioen <lodewijk.sioen@zorgi.be>
2024-04-03 10:04:51 +0200

    Split into files

---
+++ ReplaceTextInStream.Benchmark/BenchmarkInWebContext.cs
@@ -0,0 +1,65 @@
+﻿using Alba;
+using BenchmarkDotNet.Attributes;
+
+namespace ReplaceTextInStream.Benchmark;
+
+public class BenchmarkInWebContext
+{
+    //[Benchmark]
+    //public async Task StreamReader()
+    //{
+    //    await _host.Scenario(s =>
+    //    {
+    //        s.Get.Url("/stream");
+    //        s.StatusCodeShouldBeOk();
+    //        s.ContentShouldNotContain("lorem");
+    //    });
+    //}
+
+    [Benchmark(Baseline = true)]
+    public async Task Pipes()
+    {
+        await _host.Scenario(s =>
+        {
+            s.Get.Url("/pipes");
+            s.StatusCodeShouldBeOk();
+        });
+    }
+
+    //[Benchmark]
+    //public async Task Regex()
+    //{
+    //    await _host.Scenario(s =>
+    //    {
+    //        s.Get.Url("/regex");
+    //        s.StatusCodeShouldBeOk();
+    //        s.ContentShouldNotContain("lorem");
+    //    });
+    //}
+
+
+    [Benchmark]
+    public async Task String()
+    {
+        await _host.Scenario(s =>
+        {
+            s.Get.Url("/string");
+            s.StatusCodeShouldBeOk();
+        });
+    }
+
+
+    private IAlbaHost _host = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _host = AlbaHost.For<Web.Program>().GetAwaiter().GetResult();
+    }
+
+    [GlobalCleanup]
+    public void Cleanup()
+    {
+        _host.Dispose();
+    }
+}
--- ReplaceTextInStream.Benchmark/BenchmarkStreamingReplace.cs
+++ ReplaceTextInStream.Benchmark/BenchmarkStreamingReplace.cs
@@ -1,68 +1,6 @@
-﻿using Alba;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 
 namespace ReplaceTextInStream.Benchmark;
-
-public class BenchmarkInWebContext
-{
-    //[Benchmark]
-    //public async Task StreamReader()
-    //{
-    //    await _host.Scenario(s =>
-    //    {
-    //        s.Get.Url("/stream");
-    //        s.StatusCodeShouldBeOk();
-    //        s.ContentShouldNotContain("lorem");
-    //    });
-    //}
-
-    [Benchmark(Baseline = true)]
-    public async Task Pipes()
-    {
-        await _host.Scenario(s =>
-        {
-            s.Get.Url("/pipes");
-            s.StatusCodeShouldBeOk();
-        });
-    }
-
-    //[Benchmark]
-    //public async Task Regex()
-    //{
-    //    await _host.Scenario(s =>
-    //    {
-    //        s.Get.Url("/regex");
-    //        s.StatusCodeShouldBeOk();
-    //        s.ContentShouldNotContain("lorem");
-    //    });
-    //}
-
-
-    [Benchmark]
-    public async Task String()
-    {
-        await _host.Scenario(s =>
-        {
-            s.Get.Url("/string");
-            s.StatusCodeShouldBeOk();
-        });
-    }
-
-
-    private IAlbaHost _host = null!;
-
-    [GlobalSetup]
-    public void Setup()
-    {
-        _host = AlbaHost.For<Web.Program>().GetAwaiter().GetResult();
-    }
-
-    [GlobalCleanup]
-    public void Cleanup()
-    {
-        _host.Dispose();
-    }
-}
 
 public class BenchmarkStreamingReplace
 {
@@ -90,13 +28,14 @@ public class BenchmarkStreamingReplace
         await new UsingRegexReplace().Replace(input, output, "lorem", "schmorem");
     }
 
-    [Benchmark]
-    public async Task StringReplace()
-    {
-        await using var input = OpenInputStream();
-        await using var output = OpenOutputStream();
-        await new UsingStringReplace().Replace(input, output, "lorem", "schmorem");
-    }
+    //[Benchmark]
+    //public async Task StringReplace()
+    //{
+    //    await using var input = OpenInputStream();
+    //    await using var output = OpenOutputStream();
+    //    await new UsingStringReplace().Replace(input, output, "lorem", "schmorem");
+    //}
+
     private Stream OpenInputStream()
     {
         return File.OpenRead("LoremIpsum.txt");
